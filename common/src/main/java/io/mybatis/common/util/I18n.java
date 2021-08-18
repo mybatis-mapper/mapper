92cbab9891fa23eb98e09c096fef51d64d6849c9
abel533 <abel533@gmail.com>
2021-08-18 19:38:06 +0800

    重构多语言组件，解决没有默认语言包的问题，简化语言包的使用

--- common/src/main/java/io/mybatis/common/util/I18n.java
+++ common/src/main/java/io/mybatis/common/util/I18n.java
@@ -17,10 +17,9 @@
 package io.mybatis.common.util;
 
 import java.text.MessageFormat;
-import java.util.Map;
+import java.util.Locale;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
-import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * 多语言支持
@@ -30,7 +29,43 @@ import java.util.concurrent.ConcurrentHashMap;
  * @author liuzh
  */
 public class I18n {
-  public static volatile Map<String, ResourceBundle> RESOURCE_BUNDLE_MAP = new ConcurrentHashMap<>();
+
+  /**
+   * 语言包
+   */
+  public interface Language {
+    /**
+     * 获取对应语言的文本
+     *
+     * @param key
+     * @param args
+     * @return
+     */
+    String message(String key, Object... args);
+  }
+
+  /**
+   * 获取对应语言的文本，当资源文件或key不存在时，直接返回 {@code MessageFormat.format(key, args)}
+   *
+   * @param locale     语言
+   * @param bundleName 资源文件名
+   * @param key        字符串key
+   * @param args       格式化参数
+   * @return 格式化文本
+   */
+  public static String message(Locale locale, String bundleName, String key, Object... args) {
+    ResourceBundle bundle;
+    try {
+      bundle = ResourceBundle.getBundle(bundleName, locale);
+    } catch (Exception e) {
+      bundle = null;
+    }
+    try {
+      return MessageFormat.format(bundle.getString(key), args);
+    } catch (MissingResourceException e) {
+      return MessageFormat.format(key, args);
+    }
+  }
 
   /**
    * 获取对应语言的文本，当资源文件或key不存在时，直接返回 {@code MessageFormat.format(key, args)}
@@ -41,28 +76,28 @@ public class I18n {
    * @return 格式化文本
    */
   public static String message(String bundleName, String key, Object... args) {
-    if (!RESOURCE_BUNDLE_MAP.containsKey(bundleName)) {
-      synchronized (RESOURCE_BUNDLE_MAP) {
-        if (!RESOURCE_BUNDLE_MAP.containsKey(bundleName)) {
-          ResourceBundle bundle;
-          try {
-            bundle = ResourceBundle.getBundle(bundleName);
-          } catch (Exception e) {
-            bundle = null;
-          }
-          RESOURCE_BUNDLE_MAP.put(bundleName, bundle);
-        }
-      }
-    }
-    ResourceBundle bundle = RESOURCE_BUNDLE_MAP.get(bundleName);
-    if (bundle == null) {
-      return MessageFormat.format(key, args);
-    }
-    try {
-      return MessageFormat.format(bundle.getString(key), args);
-    } catch (MissingResourceException e) {
-      return MessageFormat.format(key, args);
-    }
+    return message(Locale.getDefault(), bundleName, key, args);
+  }
+
+  /**
+   * 获取语言包
+   *
+   * @param locale     语言
+   * @param bundleName 语言包名称
+   * @return
+   */
+  public static Language language(Locale locale, String bundleName) {
+    return (key, args) -> message(locale, bundleName, key, args);
+  }
+
+  /**
+   * 获取语言包
+   *
+   * @param bundleName 语言包名称
+   * @return
+   */
+  public static Language language(String bundleName) {
+    return language(Locale.getDefault(), bundleName);
   }
 
 }
