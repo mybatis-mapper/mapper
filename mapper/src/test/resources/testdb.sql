5fdab0cafbe1b0e4762e7e8ff717163bb9075978
abel533 <abel533@gmail.com>
2022-08-23 23:44:39 +0800

    更新授权文件头

--- mapper/src/test/resources/testdb.sql
+++ mapper/src/test/resources/testdb.sql
@@ -64,13 +64,29 @@ values (1, '张无忌', '男'),
        (52, '陈友谅', '男'),
        (53, '韩千叶', '男');
 
+/*
+ * Copyright 2020-2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 -- 联合主键
 create table user_ids
 (
-    id1  INTEGER,
-    id2  INTEGER,
-    name VARCHAR(32) DEFAULT 'DEFAULT',
-    PRIMARY KEY (id1, id2)
+  id1  INTEGER,
+  id2  INTEGER,
+  name VARCHAR(32) DEFAULT 'DEFAULT',
+  PRIMARY KEY (id1, id2)
 );
 
 insert into user_ids(id1, id2, name)
